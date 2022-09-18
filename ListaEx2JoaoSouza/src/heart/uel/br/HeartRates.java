1d7527e66e44ac58b349d9b64c7678b1c752e6ba
João Paulo Cardoso <joaopaulocs09@gmail.com>
2022-09-18 03:47:49 -0300

    HeartRates

---
+++ ListaEx2JoaoSouza/src/heart/uel/br/HeartRates.java
@@ -0,0 +1,55 @@
+package heart.uel.br;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class HeartRates {
+	private String nome;
+	private String sobrenome;
+	
+	private int dia;
+	private int mes;
+	private int ano;
+	
+	private int idade;
+	
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setSobrenome(String novoSobrenome) {
+		this.sobrenome = novoSobrenome;
+	}
+	
+	public String getSobrenome() {
+		return this.sobrenome;
+	}
+	
+	public void setBirthday(int novoDia, int novoMes, int novoAno) {
+		this.ano = novoAno;
+		this.mes = novoMes;
+		this.dia = novoDia;
+	}
+	
+	public int getIdade() {
+		this.idade = Period.between(LocalDate.of(ano, mes, dia), LocalDate.now()).getYears();
+		return idade;
+	}
+	
+	public int getFrequenciaMaxima() {
+		return 220 - idade;
+	}
+	
+	public double getAlvoMin() {
+		return (220 - idade) * 1.5;
+	}
+	
+	public double getAlvoMax() {
+		return (220 - idade) * 1.85;
+	}
+}
---
+++ ListaEx2JoaoSouza/src/heart/uel/br/TesteHeart.java
@@ -0,0 +1,40 @@
+package heart.uel.br;
+
+import java.util.Scanner;
+
+public class TesteHeart {
+
+	public static void main(String[] args) {
+		HeartRates hr = new HeartRates();
+		Scanner input = new Scanner(System.in);
+		String nome, sobrenome;
+		int dia, mes, ano;
+		
+		System.out.print("Insira seu nome: ");
+		nome = input.nextLine();
+		hr.setNome(nome);
+		
+		
+		System.out.print("Insira seu sobrenome: ");
+		sobrenome = input.nextLine();
+		hr.setSobrenome(sobrenome);
+		
+		
+		System.out.print("Insira o dia em que você nasceu: ");
+		dia = input.nextInt();
+		System.out.print("Insira o mes em que você nasceu: ");
+		mes = input.nextInt();
+		System.out.print("Insira o ano em que você nasceu: ");
+		ano = input.nextInt();
+				
+		hr.setBirthday(dia, mes, ano);
+		
+		System.out.println("=========== Heart Rates ===============");	
+		System.out.println(hr.getNome() + " " + hr.getSobrenome() + " - " + hr.getIdade() +" anos");
+		System.out.println("Frequencia cardiaca maxima: " + hr.getFrequenciaMaxima());
+		System.out.println("Frequencia cardiaca alvo: " + hr.getAlvoMin() + " ~ " + hr.getAlvoMax());
+		
+		input.close();
+	}
+
+}
