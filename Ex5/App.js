520b13598582188b6af490023bc761dad4773faf
João Paulo Cardoso <joaopaulocs09@gmail.com>
2023-04-11 21:02:05 -0300

    Exercicio 5

---
+++ Ex5/App.js
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text,Image, View } from 'react-native';
+import Button from './Button';
+import ImageViewer from './ImageViewer';
+
+const PlaceholderImage = require('./assets/background-image.png');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
